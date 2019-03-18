a0eceb3b12c90d274bd54c15d079c592b45e50c4
Ernestodiazxd <xdiaz17@jaumebalmes.net>
2019-03-18 16:36:02 +0100

    v0.2

--- app/src/main/java/com/example/xdiaz/uf3activitat1/GamePAD.java
+++ app/src/main/java/com/example/xdiaz/uf3activitat1/GamePAD.java
@@ -1,85 +1,104 @@
 package com.example.xdiaz.uf3activitat1;
 
 import android.graphics.Color;
+import android.support.v4.view.TintableBackgroundView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class GamePAD extends AppCompatActivity {
 
     Button barriba,babajo,bderecha,bizquierda;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_pad);
-
+        text=findViewById(R.id.TextGamePAD);
         barriba= findViewById(R.id.buttonArriba);
         babajo= findViewById(R.id.buttonAbajo);
         bderecha= findViewById(R.id.buttonDerecha);
         bizquierda= findViewById(R.id.buttonIzquierda);
-        
-       barriba.setOnTouchListener(new View.OnTouchListener() {  
+        text.setText("Null");
+
+
+        barriba.setOnTouchListener(new View.OnTouchListener() {
         @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    barriba.setBackgroundColor(Color.BLUE);
+                    barriba.setBackgroundColor(getResources().getColor(R.color.blue));
+                    text.setText("Arriba");
+
                 }
                 if(event.getAction() == MotionEvent.ACTION_UP){
-                    barriba.setBackgroundColor(Color.RED);
+                    barriba.setBackgroundColor(getResources().getColor(R.color.red));
+                    text.setText("Null");
+
                 }
                 return true;
             }
 
         });
-}
+
     
      babajo.setOnTouchListener(new View.OnTouchListener() {  
         @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    babajo.setBackgroundColor(Color.BLUE);
+                    babajo.setBackgroundColor(getResources().getColor(R.color.blue));
+                    text.setText("Abajo");
                 }
                 if(event.getAction() == MotionEvent.ACTION_UP){
-                    abajo.setBackgroundColor(Color.RED);
+                    babajo.setBackgroundColor(getResources().getColor(R.color.red));
+                    text.setText("Null");
+
                 }
                 return true;
             }
 
         });
-}
+
 
  bderecha.setOnTouchListener(new View.OnTouchListener() {  
         @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    bderecha.setBackgroundColor(Color.BLUE);
+                    bderecha.setBackgroundColor(getResources().getColor(R.color.blue));
+                    text.setText("Derecha");
                 }
                 if(event.getAction() == MotionEvent.ACTION_UP){
-                    bderecha.setBackgroundColor(Color.RED);
+                    bderecha.setBackgroundColor(getResources().getColor(R.color.red));
+                    text.setText("Null");
+
                 }
                 return true;
             }
 
         });
-}
+
 
  bizquierda.setOnTouchListener(new View.OnTouchListener() {  
         @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    bizquierda.setBackgroundColor(Color.BLUE);
+                    bizquierda.setBackgroundColor(getResources().getColor(R.color.blue));
+                    text.setText("Izquierda");
                 }
                 if(event.getAction() == MotionEvent.ACTION_UP){
-                    bizquierda.setBackgroundColor(Color.RED);
+                    bizquierda.setBackgroundColor(getResources().getColor(R.color.red));
+                    text.setText("Null");
+
                 }
                 return true;
             }
 
         });
 }
+
 
     /*
     
--- app/src/main/java/com/example/xdiaz/uf3activitat1/KeyTouch.java
+++ app/src/main/java/com/example/xdiaz/uf3activitat1/KeyTouch.java
@@ -2,12 +2,56 @@ package com.example.xdiaz.uf3activitat1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class KeyTouch extends AppCompatActivity {
+    ImageView arriba,abajo;
+    TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_key_touch);
+        arriba=findViewById(R.id.Arriba);
+        abajo=findViewById(R.id.Abajo);
+        texto=findViewById(R.id.volumen);
+        arriba.setVisibility(View.INVISIBLE);
+        abajo.setVisibility(View.INVISIBLE);
+
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            texto.setVisibility(View.INVISIBLE);
+            arriba.setVisibility(View.INVISIBLE);
+            abajo.setVisibility(View.VISIBLE);
+            // Codi de baixar volum
+            return true;
+
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+
+        if(keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            texto.setVisibility(View.INVISIBLE);
+            arriba.setVisibility(View.VISIBLE);
+            abajo.setVisibility(View.INVISIBLE);
+            // Codi de pujar volum
+            return true;
+
+        }
+
+        return super.onKeyUp(keyCode, event);
     }
 }
